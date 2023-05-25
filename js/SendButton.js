c1b07cbb676e17721452411e758b129b2df9391a
Cybot <stancilaionutmarian@yahoo.com>
2023-05-25 11:29:40 +0300

    folosirea a cel puțin o metodă din clasele Math, Array, String + LocalStorage

--- js/SendButton.js
+++ js/SendButton.js
@@ -20,28 +20,50 @@ function onClickEvent(){
 let formular=document.getElementById("contactForm");
 console.log(formular);
 
-formular.addEventListener("submit", function(event){
+formular.addEventListener("submit", function(event) {
     event.preventDefault();
-
+  
     var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value; 
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
     var currentDate = new Date().toLocaleString();
-    
+  
     const namePattern = /^[A-Z][a-zA-Z]+(?:\s+[A-Z][a-zA-Z]+)*$/;
-
+  
     if (!namePattern.test(name)) {
-        alert('Introduceți un nume de persoană valid.');
-        document.getElementById('name').value = '';
-    return;
+      alert('Introduceți un nume de persoană valid.');
+      document.getElementById('name').value = '';
+      return;
     }
-    const para=document.createElement("p");
-
-    para.textContent="Name: "+name+" Email: "+email+ " Submitted on: "+currentDate;
-
-    const container=document.getElementById("formInfo");
+  
+    const para = document.createElement("p");
+    para.textContent = "Name: " + name + " Email: " + email + " Submitted on: " + currentDate;
+  
+    const container = document.getElementById("formInfo");
     container.appendChild(para);
+  
+    var formData = "Name: " + name + ", Email: " + email + ", Message: " + message + ", Submitted on: " + currentDate;
+  
+    // Am generat un nr aleator de la 1 la 100 :p
+    var randomNum = Math.floor(Math.random() * 100) + 1;
+    console.log("Random number:", randomNum);
+  
+    // folosesc replace pentru a maska emailul
+    var maskedFormData = formData.replace(/Email/g, "***");
+    console.log("Masked form data:", maskedFormData);
 
-    document.getElementById("name").value="";
-    document.getElementById("email").value="";
-    document.getElementById("message").value=""; 
+    var storedFormData = localStorage.getItem("formDataItems");
+    var formDataArray = storedFormData ? storedFormData.split("\n") : [];
+
+    formDataArray.push(formData);
+
+    localStorage.setItem("formDataItems", formDataArray.join("\n"));
+    
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
 });
+
+window.addEventListener("beforeunload", ()=>{
+    localStorage.removeItem("formDataItems");
+})
