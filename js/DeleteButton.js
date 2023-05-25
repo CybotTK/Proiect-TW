39aac0f7e73a2887af89d243351a9a3e522443d0
Cybot <stancilaionutmarian@yahoo.com>
2023-05-25 11:41:50 +0300

    background color random change

--- js/DeleteButton.js
+++ js/DeleteButton.js
@@ -1,4 +1,14 @@
-function deleteForm(){
+function deleteForm(event){
     const para=document.getElementById("formInfo");
     para.lastChild.remove();
+
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    
+    const fonts = ["Arial", "Verdana", "Helvetica", "Tahoma", "Times New Roman", "Courier New"];
+    
+    const randomFont = fonts[Math.floor(Math.random() * fonts.length)];
+
+    const deleteButton = event.target;
+    deleteButton.style.color = randomColor;
+    deleteButton.style.fontFamily = randomFont;
 }
