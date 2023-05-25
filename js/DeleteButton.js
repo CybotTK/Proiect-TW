e0ce2d58f44abd8b25fe87d52aeec70276106c0f
Cybot <stancilaionutmarian@yahoo.com>
2023-05-25 11:58:30 +0300

    more js code

--- js/DeleteButton.js
+++ js/DeleteButton.js
@@ -7,8 +7,4 @@ function deleteForm(event){
     const fonts = ["Arial", "Verdana", "Helvetica", "Tahoma", "Times New Roman", "Courier New"];
     
     const randomFont = fonts[Math.floor(Math.random() * fonts.length)];
-
-    const deleteButton = event.target;
-    deleteButton.style.color = randomColor;
-    deleteButton.style.fontFamily = randomFont;
 }
